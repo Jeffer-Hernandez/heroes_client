9d8d13db243b32160bcc23497b7f51772c383b65
github username <jhernandez1692@gmail.com>
2021-04-18 17:46:02 -0400

    add notes and dispatch function name

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
 
 // const mapStateToProps (state) =>{
 //   return {
-//     accounts: state.accounts
+//     heroes: state.heroes
 //   }
 // }
 
--- src/actions/fetchHeroes.js
+++ src/actions/fetchHeroes.js
@@ -3,4 +3,7 @@ export function fetchHeroes() {
     .then(resp => resp.json())
     .then(data => console.log(data))
 
+    dispatchEvent() // will dispatch to the reducer, which will update the store
+                    // can be dispatched within this action because of thunk
+
 }
