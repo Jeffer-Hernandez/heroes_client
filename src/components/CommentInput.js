9ee8cd2c1acc896bb7c9bab2dade618ec131d815
github username <jhernandez1692@gmail.com>
2021-04-20 17:29:40 -0400

    Created a comment form.

---
+++ src/components/CommentInput.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class CommentInput extends React.Component {
+
+    render(){
+        return(
+
+            <div>
+                Comment Input
+            </div>
+
+
+        )
+
+    }
+}
+
+export default CommentInput
