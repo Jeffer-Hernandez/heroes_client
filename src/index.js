c59cf4e35b9c3f0e7f1cc629f73b44528f0ca260
github username <jhernandez1692@gmail.com>
2021-04-20 17:53:52 -0400

    refactor.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {Connect} from 'react-redux'
 import App from './App';
 
 import {createStore, applyMiddleware} from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
-import heroReducer from './reducers/heroReducers'
+import heroReducer from './reducers/commentReducer'
 
 
 
@@ -20,4 +20,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
