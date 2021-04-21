0f8be1244323486c5d48597d0f78b9477d9abec5
github username <jhernandez1692@gmail.com>
2021-04-20 21:55:28 -0400

    create fetch and dispatch calls.

--- src/actions/fetchHeroes.js
+++ src/actions/fetchHeroes.js
@@ -1,9 +1,13 @@
 export function fetchHeroes() {
-    // fetch('http://localhost:3000/api/v1/heroes')
-    // .then(resp => resp.json())
-    // .then(data => console.log(data))
-
-    // dispatchEvent() // will dispatch to the reducer, which will update the store
-                    // can be dispatched within this action because of thunk
+    return (dispatch)=>{
+        fetch('http://localhost:3000/api/v1/heros')
+        .then(resp => resp.json())
+        .then(heroes => dispatch({
+            type: 'FETCH_HEROES',
+            payload: heroes
+        })
+        )
+    }
+   
 
 }
