fc403cd052e2dd91f71f9ed292d73c7abf55fcd6
github username <jhernandez1692@gmail.com>
2021-04-21 16:39:45 -0400

    comment reducer skeleton.

---
+++ src/reducers/commentReducer.js
@@ -0,0 +1,17 @@
+
+
+//this will update the state of the store to include the newest comment
+// export default function commentReducer(state = {comments: []}, action ) {
+
+//     switch(action.type){
+     
+//      case 'POST_COMMENTS':
+//          return {comments: action.payload}
+//      default:
+//          return state
+     
+         
+ 
+//     }
+ 
+//  }
