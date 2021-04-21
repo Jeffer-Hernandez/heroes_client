c5b2b4e384c0fdb1a7ce55defcf8dd788924d0e6
github username <jhernandez1692@gmail.com>
2021-04-20 21:56:08 -0400

    connect store.

--- src/containers/HeroContainer.js
+++ src/containers/HeroContainer.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import CommentContainer from './CommentContainer'
+import {fetchHeroes} from '../actions/fetchHeroes'
+import {connect} from 'react-redux'
+import Heroes from '../components/Heroes'
 
 
 
 
 class HeroContainer extends React.Component {
 
+    componentDidMount(){
+        this.props.fetchHeroes()
+
+    }
+
     render() {
         return(
         <div className="HeroContainer">
             HeroContainer
             <CommentContainer/>
+            <Heroes heroes={this.props.heroes}/>
         </div>
         )
 
     }
 }
 
-export default HeroContainer
+const mapStateToProps = state =>{
+
+    return{
+        heroes: state.heroes
+    }
+
+}
+
+export default connect(mapStateToProps, {fetchHeroes})(HeroContainer)
