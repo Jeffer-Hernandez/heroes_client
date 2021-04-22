512921029aa948bafc31185913a67161208a3e64
github username <jhernandez1692@gmail.com>
2021-04-22 12:14:16 -0400

    connect and mapstatetoprops added in.

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React from 'react'
 
 
 import HeroContainer from './containers/HeroContainer'
-
+import {fetchHeroes} from './actions/fetchHeroes'
+import {connect} from 'react-redux'
 
 
 class App extends React.Component {
+
+  componentDidMount(){
+    debugger
+    this.props.fetchHeroes()
+    
+}
 
 
 
   render(){
     return (
       <div className="App">
-       <HeroContainer/>
+       <HeroContainer heroes={this.props.heroes.data}/>
         
       </div>
     );
   }
 }
 
+const mapStateToProps = state =>{
 
-export default App
+  return{
+      heroes: state.heroes
+  }
+
+}
+
+export default connect(mapStateToProps, {fetchHeroes}) (App)
