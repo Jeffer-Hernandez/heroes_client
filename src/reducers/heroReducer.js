ddfc4f65f7147978b54d19212a732c6297e562eb
github username <jhernandez1692@gmail.com>
2021-04-22 18:31:49 -0400

    debugging.

--- src/reducers/heroReducer.js
+++ src/reducers/heroReducer.js
@@ -1,8 +1,9 @@
 export default function heroReducer(state = {heroes: []}, action ) {
-    debugger
+  
    switch(action.type){
     
     case 'FETCH_HEROES':
+        debugger
         return {heroes: action.payload}
     default:
         return state
