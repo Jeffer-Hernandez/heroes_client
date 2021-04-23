fde870ba92e1af6df7ce1c03c5ce2ef3cc5c9333
github username <jhernandez1692@gmail.com>
2021-04-23 19:56:16 -0400

    created comment form and local state.

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -2,11 +2,31 @@ import React from 'react'
 
 class CommentInput extends React.Component {
 
+    state = {
+        content: '',
+        hero_id: this.props.heroId
+}
+
+    handleChange = (event) =>{
+
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = () =>{
+
+    }
+
+
     render(){
         return(
 
             <div className="CommentInput">
-                Comment Input
+                <form onSubmit={this.handleSubmit}>
+                    <input type= "text" placeholder="What's on your mind?" value={this.state.content} name="content" onChange={this.handleChange}/><br/>
+                    <input type="submit"/>
+                </form>
             </div>
 
 
