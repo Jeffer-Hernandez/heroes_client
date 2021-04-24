402f2225fe57a72af94ff978e6d4c1b5fbe91a7f
github username <jhernandez1692@gmail.com>
2021-04-24 19:07:58 -0400

    Wrapped router around app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
-
 import {createStore, applyMiddleware} from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 import heroReducer from './reducers/heroReducer'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 
 
@@ -16,7 +15,9 @@ let store = createStore(heroReducer, composeWithDevTools(applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
