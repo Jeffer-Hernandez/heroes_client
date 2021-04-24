866b6e6f0bd737fa2c676c376492b3ed3a303b64
github username <jhernandez1692@gmail.com>
2021-04-23 20:49:34 -0400

    post fetch for comments.

--- src/actions/postComments.js
+++ src/actions/postComments.js
@@ -1,15 +1,24 @@
 
-// this will be a post request to persist comments
-// export function postComments() {
-//     return (dispatch)=>{
-//         fetch('http://localhost:3000/api/v1/comment')
-//         .then(resp => resp.json())
-//         .then( => dispatch({
-//             type: 'POST_COMMENTS',
-//             payload: heroes
-//         })
-//         )
-//     }
+
+export const postComments = (data) => {
+
+    return (dispatch)=>{
+        fetch('http://localhost:3000/api/v1/comments', {
+            headers: {
+                'Content_Type': 'application/json',
+                'Accept': 'content/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+
+        })
+        .then(resp => resp.json())
+        .then(comment => dispatch({
+            type: 'POST_COMMENT',
+            payload: comment
+        })
+        )
+    }
    
 
-// }
+}
