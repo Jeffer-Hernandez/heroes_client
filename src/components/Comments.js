925aaf3a72cd8a9f9c246c9542b2e52ffdbb2030
github username <jhernandez1692@gmail.com>
2021-04-24 13:58:19 -0400

    dynamically render individual comments.

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,17 +1,15 @@
 import React from 'react'
+import CommentCard from './CommentCard'
 
 
 const Comments = (props) => {
 
-
-    return(
-        <div className="Comments">
-            {/* make comment content render */}
-            {props.map(comment=> <li>{comment.content}</li>)}
-
-        </div>
-    )
-
+    return props.heroComments.map(function(comment) {
+        // debugger
+        console.log(comment.content)
+        return <CommentCard userComment={comment.content}/>
+    })
+  
 }
 
 export default Comments
