52725beafcb71de2e3516368db8d70ac5047bcad
github username <jhernandez1692@gmail.com>
2021-04-24 19:07:42 -0400

    remove connect and fetch requests.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
 import React from 'react'
-
-
+import {Route} from 'react-router-dom'
 import HeroContainer from './containers/HeroContainer'
-import {fetchHeroes} from './actions/fetchHeroes'
-import {connect} from 'react-redux'
 
 
 class App extends React.Component {
 
-  componentDidMount(){
- 
-    this.props.fetchHeroes()
-    
-  }
-
-
-
   render(){
     return (
       <div className="App">
-       <HeroContainer heroes={this.props.heroes.data}/>
-        
+       <Route path='/heroes' component={HeroContainer}/>
       </div>
     );
   }
 }
 
-const mapStateToProps = state =>{
 
-  return{
-      heroes: state.heroes
-  }
-
-}
-
-export default connect(mapStateToProps, {fetchHeroes}) (App)
+export default App
