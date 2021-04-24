1f338644f2b80fff4bffca974903b4bfbff28c67
github username <jhernandez1692@gmail.com>
2021-04-24 19:10:03 -0400

    fetching and mapstatetoprops in this comp.

--- src/containers/HeroContainer.js
+++ src/containers/HeroContainer.js
@@ -2,36 +2,43 @@ import React from 'react'
 import {fetchHeroes} from '../actions/fetchHeroes'
 import {connect} from 'react-redux'
 import HeroCard from '../components/HeroCard'
+import {Route} from 'react-router-dom'
+import HeroShow from '../components/HeroShow'
+
 
 
 
 
 class HeroContainer extends React.Component {
 
-   
+    componentDidMount(){
+ 
+        this.props.fetchHeroes()
+        
+    }
 
     
     heroData = () => {
-
-
+        console.log(this.props.heroes)
         return this.props.heroes.map(function(hero) {
             // debugger
-            console.log(hero.attributes.comments)
-            return <HeroCard heroId={hero.id} heroName={hero.attributes.name} heroDescription={hero.attributes.description} 
+            // console.log(this.props.heroes)
+            return <HeroCard heroId={hero.id} heroName={hero.attributes.name} heroDescription={hero.attributes.description}
             heroImage={hero.attributes.image_url} heroComments={hero.attributes.comments}/>
         })
-
         
     }
 
     render() {
   
-        
-            
         return(
         <div className="HeroContainer">
             
            {this.heroData()}
+           <Route path='/heroes/:id' render={(routerProps)=> <HeroShow {...routerProps} heroes={this.props.heroes}/>}/>
+
+           {/* route for heroShow at /heroes/id? */}
+           {/* see how links are made, make a link from superhero picture to heroShow */}
               
         </div>
         )
