79eacae0d85163cb2db0d030d37e00e80fc9d9d4
github username <jhernandez1692@gmail.com>
2021-04-24 21:08:48 -0400

    include route for show

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Route} from 'react-router-dom'
+import HeroShow from '../components/HeroShow'
 
 
 
@@ -10,7 +12,8 @@ const HeroCard = (props) =>{
         <div className="HeroCard">
             {<h1>{props.heroName}</h1>}
             {<h1>{props.heroImage}</h1>}
-            {<h1>{props.heroDescription}</h1>}            
+            {<h1>{props.heroDescription}</h1>}      
+            <Route path='/heroes/:id' render={(routerProps)=> <HeroShow {...routerProps} heroes={props}/>}/>      
         </div>
     )
 
