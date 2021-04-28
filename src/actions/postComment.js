f809ec826bdda0414fd11d5a6ad545b9742db4a0
github username <jhernandez1692@gmail.com>
2021-04-28 14:03:05 -0400

    redid post comment

---
+++ src/actions/postComment.js
@@ -0,0 +1,24 @@
+
+
+export const postComment = (data) => {
+
+    return (dispatch)=>{
+        fetch('http://localhost:3000/api/v1/comments', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+
+        })
+        .then(response => response.json())
+        .then(comment => dispatch({
+            type: 'POST_COMMENT',
+            payload: comment
+        })
+        )
+    }
+   
+
+}
