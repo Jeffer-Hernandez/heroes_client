e92452f58f6d9ff50b9ada40a811a767c9a0cb5a
github username <jhernandez1692@gmail.com>
2021-04-28 14:03:18 -0400

    remove console.log

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -3,8 +3,6 @@ import React from 'react'
 
 const CommentCard = (props) =>{
 
-    console.log(props)
-
     return(
         <div key={props.userComment.id} className="HeroCard">
             {<p>{props.userComment}</p>}
