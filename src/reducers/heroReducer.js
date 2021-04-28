72d0d266d5f380b06b4ab8f9408d170b96252084
github username <jhernandez1692@gmail.com>
2021-04-28 14:06:10 -0400

    implemented a post_comment reducer.

--- src/reducers/heroReducer.js
+++ src/reducers/heroReducer.js
@@ -1,13 +1,41 @@
 export default function heroReducer(state = {heroes: []}, action ) {
-  
+    
+    let idx;
+    let comment;
+
    switch(action.type){
     
-    case 'FETCH_HEROES':
-        // debugger
-        return {heroes: action.payload}
-    default:
-        return state
+        case 'FETCH_HEROES':
+            // debugger
+            // return {heroes: action.payload}
+            return{
+            
+                heroes: action.payload
+                // comments: [...state.comments]
+            
+            };
 
-   }
+        case 'POST_COMMENT':
+                
+                idx = action.payload.hero_id - 1;
 
+            return{
+                ...state,
+                heroes: [...state.heroes],
+                    [idx]: [state.heroes[idx]],
+                        attributes: [state.heroes[idx].attributes],
+                            comments: state.heroes[idx].attributes.comments.push(action.payload)
+                            
+                                
+            }
+
+            
+            
+        
+        
+        default:
+            return state
+
+
+    }
 }
