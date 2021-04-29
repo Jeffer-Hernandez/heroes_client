e2ca927b8a67fa07ffeba5e1ea7b66c2831262ce
github username <jhernandez1692@gmail.com>
2021-04-29 16:51:31 -0400

    Create an about page.

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const About = () =>{
+
+    return(
+        <div  className="Welcome">
+            {<h1>Heroes is about nerding out with your peeps!</h1>}
+            {<p>A place to come and talk about the latest issue of Bataman.</p>}
+            {<p>You can let your hair down and say what's really on  your mind.</p>}
+            {<p>Talk about the art work in our favorite graphic novels!</p>}
+            {<p>Y'know, the fun stuff!</p>}
+        </div>
+    )
+
+}
+
+export default About
