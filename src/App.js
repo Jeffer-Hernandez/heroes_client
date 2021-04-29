8dd46661166ac428a8540eec0415ecd3f2d7ce0c
github username <jhernandez1692@gmail.com>
2021-04-29 16:51:17 -0400

    Include routes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
 import HeroContainer from './containers/HeroContainer'
+import Welcome from './components/Welcome'
+import About from './components/About'
 
 
 class App extends React.Component {
@@ -8,7 +10,12 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-       <Route path='/heroes' component={HeroContainer}/>
+   
+        <Route exact path='/' component={Welcome}/>
+        <Route path='/about' component={About}/>
+        <Route path='/heroes' component={HeroContainer}/>
+       
+
        {/* <h1>Welcome to Heroes!</h1> */}
        {/* make a welcome component, a link to heroContainer */}
       </div>
