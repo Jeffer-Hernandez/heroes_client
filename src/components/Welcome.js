6950fb9b66e3848d46a130428b2c801bafc24f65
github username <jhernandez1692@gmail.com>
2021-04-29 16:51:59 -0400

    created welcome.

---
+++ src/components/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Welcome = () =>{
+
+    return(
+        <div  className="Welcome">
+            {<h1>WELCOME TO HEROES!</h1>}
+            {<h1>HANG OUT AND CHAT ABOUT YOUR FAVORITES!</h1>}
+        </div>
+    )
+
+}
+
+export default Welcome
