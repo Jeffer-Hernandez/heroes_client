2691b730d68d9a8de49fe8024103a83dca7ed2b1
github username <jhernandez1692@gmail.com>
2021-05-05 15:47:42 -0400

    Included a Like button and counter.

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,14 +1,42 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 
-const CommentCard = (props) =>{
+class CommentCard extends React.Component {
 
-    return(
-        <div key={props.userComment.id} className="HeroCard">
-            {<p>{props.userComment}</p>}
-        </div>
-    )
+    state = {
+        content: '',
+        hero_id: this.props.heroId,
+        comment_id: this.props.userComment.id,
+        like_count: 0
+    }
 
+  
+
+     handleClick = (event) =>{
+        
+        event.preventDefault()
+        // this.props.postComment(this.state)
+        this.setState({
+            content: '',
+            hero_id: this.props.heroId,
+            like_count: this.state.like_count +1
+
+        })
+    }
+
+    render(){
+        const handleClick = this.handleClick
+        return(
+            <div key={this.props.userComment.id} className="CommentCard">
+                {<p>{this.props.userComment}</p>}
+                {<p>{this.state.like_count}</p>}
+                <button onClick={handleClick}>Like</button>
+            </div>
+        )
+    }
 }
 
 export default CommentCard
+
+// export default connect(null, )(CommentInput)
